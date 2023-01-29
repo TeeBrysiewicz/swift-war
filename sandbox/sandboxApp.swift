9acb679d65a1b91bed2f9d9e6cdaade3517b770e
Tobias Brysiewicz <tbrysiewicz@att.net>
2023-01-29 09:33:17 -0600

    Initial Commit

---
+++ sandbox/sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandboxApp.swift
+//  sandbox
+//
+//  Created by Tobias Brysiewicz on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
